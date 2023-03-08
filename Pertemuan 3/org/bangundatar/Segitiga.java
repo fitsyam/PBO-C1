63863657b2d36175956cb09667148d2f06fc92f2
fitsyam <100133818+fitsyam@users.noreply.github.com>
2023-03-08 19:36:02 +0700

    Add files via upload

---
+++ Pertemuan 3/org/bangundatar/Segitiga.java
@@ -0,0 +1,26 @@
+/**
+  * File : Poligon.java 08/03/2023
+  * Penulis : Fitra Syamli Yudhisaputra
+  * Deskripsi : representasi dasar dari objek segitiga turunan poligon
+  */
+package org.bangundatar;
+
+import org.poligon.Poligon;
+
+public class Segitiga extends Poligon{
+	private double alas,tinggi;
+	
+	public Segitiga(double alas, double tinggi, int jumlahSisi){
+		this.alas = alas;
+		this.tinggi = tinggi;
+		this.jumlahSisi = jumlahSisi;
+	}
+	
+	public double hitungLuas(){
+		return alas * tinggi / 2;
+	}
+	public void	printInfo(){
+		System.out.println("Bangun Segitiga bersisi "+this.getJumlahSisi());
+	}
+}
+	
